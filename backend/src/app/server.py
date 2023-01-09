3984f09753d5c183fff2cc758d38cda4fa3cf36b
ot07 <ot07neoki@gmail.com>
2023-01-10 00:29:09 +0900

    api returns note sequence in json format

--- backend/src/app/server.py
+++ backend/src/app/server.py
@@ -1,4 +1,5 @@
 import fastapi
+from google.protobuf.json_format import MessageToDict
 
 from app.generate_unconditional import generate_from_scratch
 
@@ -13,4 +14,4 @@ def root():
 @app.get("/generate/unconditional/from-scratch")
 def generate_from_scratch_with_unconditional_model():
     generated_ns = generate_from_scratch()
-    return {"generated_ns": generated_ns}
+    return MessageToDict(generated_ns)
