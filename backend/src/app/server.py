84db26b9d7abdbc4c952738612bb3e63555a38d6
ot07 <ot07neoki@gmail.com>
2023-01-11 21:16:40 +0900

    add CORS setting

--- backend/src/app/server.py
+++ backend/src/app/server.py
@@ -1,10 +1,18 @@
 import fastapi
+from fastapi.middleware.cors import CORSMiddleware
 from google.protobuf.json_format import MessageToDict
 
 from app.generate_unconditional import generate_from_scratch
 
 app = fastapi.FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
