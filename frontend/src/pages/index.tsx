7380f75231e44df5e0f34dcb325fceec7705b5b9
ot07 <ot07neoki@gmail.com>
2023-04-02 21:18:37 +0900

    remove env debug code

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,11 +3,6 @@ import { UnconditionalGenerate } from "../components/UnconditionalGenerate";
 import { Center, Container, Stack, Title } from "@mantine/core";
 
 export default function Home(): JSX.Element {
-  console.log("test:", import.meta.env.VITE_API_URL);
-  console.log("test2:", import.meta.env.API_URL);
-  console.log("test3:", import.meta.env.VITE_TEST);
-  console.log("test4:", import.meta.env.TEST);
-  console.log("test4:", import.meta.env);
   return (
     <MainLayout title="Music Transformer Playground">
       <Container py={80}>
