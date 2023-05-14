5e7813902a9c2b0e81cf757d134e33a02309869b
ot07 <ot07neoki@gmail.com>
2023-05-15 02:15:08 +0900

    adjust all notes so that first note ticks is 0

--- frontend/src/components/UnconditionalGenerate/UnconditionalGenerate.tsx
+++ frontend/src/components/UnconditionalGenerate/UnconditionalGenerate.tsx
@@ -8,6 +8,7 @@ import { useQuery } from "@tanstack/react-query";
 import { type FC, useLayoutEffect, useMemo, useState } from "react";
 import {
   createSongFromMidiArrayBuffer,
+  Ticks,
   type Song,
 } from "@resonance-box/recital-core";
 import { getMinMaxNoteNumber } from "../../utils/getMinMaxNoteNumber";
@@ -15,7 +16,19 @@ import { getMinMaxNoteNumber } from "../../utils/getMinMaxNoteNumber";
 async function getGeneratedSong(apiUrl: string): Promise<Song> {
   const response = await fetch(`${apiUrl}/generate/unconditional/from-scratch`);
   const data = await response.arrayBuffer();
-  return createSongFromMidiArrayBuffer(data, 480);
+  const song = createSongFromMidiArrayBuffer(data, 480);
+
+  const track = song.getTracks()[0];
+  const notes = track.sortedNotes;
+
+  const firstNoteTicks = notes[0].ticks.value;
+  notes.forEach((note) => {
+    track.updateNote(note.id, {
+      ticks: new Ticks(note.ticks.value - firstNoteTicks),
+    });
+  });
+
+  return song;
 }
 
 interface GenerateButtonProps {
