887166784ca63cef2b7ffcd4a6bae30bbad36bb8
ot07 <ot07neoki@gmail.com>
2023-08-03 22:36:41 +0900

    add linear scale velocities process

--- web/src/components/Generator/getGeneratedSong.ts
+++ web/src/components/Generator/getGeneratedSong.ts
@@ -2,7 +2,32 @@ import {
   createSongFromMidiArrayBuffer,
   Ticks,
   Song,
+  Track,
+  Velocity,
 } from "@resonance-box/recital-core";
+
+function linearScaleVelocities(track: Track, min: number, max: number) {
+  const notes = track.sortedNotes;
+
+  const velocities = notes.map((note) => note.velocity.value);
+  const minVelocity = Math.min(...velocities);
+  const maxVelocity = Math.max(...velocities);
+
+  notes.forEach((note) => {
+    const normalizedVelocity =
+      (note.velocity.value - minVelocity) / (maxVelocity - minVelocity);
+    const remappedVelocity = min + normalizedVelocity * (max - min);
+
+    console.log({
+      velocity: note.velocity.value,
+      scaledVelocity: Math.round(remappedVelocity),
+    });
+
+    track.updateNote(note.id, {
+      velocity: new Velocity(Math.round(remappedVelocity)),
+    });
+  });
+}
 
 export async function getGeneratedSong(apiUrl: string): Promise<Song> {
   const response = await fetch(`${apiUrl}/generate/unconditional/from-scratch`);
@@ -18,6 +43,8 @@ export async function getGeneratedSong(apiUrl: string): Promise<Song> {
       ticks: new Ticks(note.ticks.value - firstNoteTicks),
     });
   });
+
+  linearScaleVelocities(track, 80, 127);
 
   return song;
 }
